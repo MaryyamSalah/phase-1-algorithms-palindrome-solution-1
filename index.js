4aa750d98ba787f75f53347c4a1e90dee329f9c3
MaryyamSalah <maryyamslah20@gmail.com>
2022-11-23 11:57:48 +0300

    completed asiignmnet

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-function isPalindrome(word) {
+function reversePalindrome(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("")
 }
 
+function isPalindrome(word){
+
+const reverseString = reversePalindrome(word)
+
+  return (word === reverseString)
+}
+
+
 /* 
-  Add your pseudocode here
+   // create an array from the input string
+     // reverse the array
+     // create a string from the reversed array
+      // return the reversed string
 */
 
 /*
-  Add written explanation of your solution here
+// reverse the input string
+  // if the reversed string is the same as the input
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+  
+  console.log(""); 
+
+  
+
+  console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
